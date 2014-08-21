f8ff8d0b198d36091ec289983b20d5cc452d8e4f
paalsteek <github@paalsteek.de>
2014-08-21 12:15:43 +0200

    heartbeat added

---
+++ src/heartbeat.cpp
@@ -0,0 +1,38 @@
+/**
+ * This file is part of libmysmartgrid.
+ *
+ * (c) Fraunhofer ITWM - Mathias Dalheimer <dalheimer@itwm.fhg.de>,    2010
+ *                       Ely de Oliveira   <ely.oliveira@itwm.fhg.de>, 2013
+ *                       Stephan Platz     <platz@itwm.fhg.de>,        2014
+ *
+ * libklio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libklio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libklio. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+#include <libmysmartgrid/webclient.h>
+#include <libmysmartgrid/error.h>
+
+int main(int argc, char* argv[]) {
+	Json::Value *value = new Json::Value(Json::objectValue);
+	(*value)["key"] = "644bc984759564991ccabe7f9fcb801a";
+	(*value)["uptime"] = 0;
+	(*value)["version"] = 0;
+
+	try {
+		boost::shared_ptr<Json::Value> v = libmsg::Webclient::performHttpRequest("POST", "https://dev3-api.mysmartgrid.de:8443/device/ae44fe656e58a43284995e5db583b378", "644bc984759564991ccabe7f9fcb801a", boost::shared_ptr<Json::Value>(value));
+		std::cout << "Result: " << *v << std::endl;
+	} catch ( const libmsg::GenericException& e ) {
+		std::cout << "Error during request: " << e.what();
+	}
+}
